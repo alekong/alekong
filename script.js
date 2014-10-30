153c9a3acf1536f89c7fd761c9bc1ec92f654956
alekong <aleksskole@gmail.com>
2014-10-30 17:34:37 +0100

    Remove jQuery dependency and beautify code
    Also the code is world class code now

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-$(document).ready(function(){var audioElement=document.createElement('audio');audioElement.setAttribute('src','http://a.pomf.se/fgxbrl.wav');audioElement.setAttribute('autoplay','autoplay');audioElement.addEventListener("load",function(){audioElement.play();},true);});
+document.addEventListener('DOMContentLoaded', function() {
+    var audioElement = document.createElement('audio');
+    audioElement.setAttribute('src', 'http://a.pomf.se/fgxbrl.wav');
+    audioElement.setAttribute('autoplay', 'autoplay');
+    audioElement.addEventListener("load", function() {
+        audioElement.play();
+    }, true);
+});
